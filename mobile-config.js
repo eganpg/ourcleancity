abf867b228692c2ab48ebc5fe7f3cfc9d6743c61
eganpg <eganpg@gmail.com>
2015-01-18 12:45:17 -0800

    android image

--- mobile-config.js
+++ mobile-config.js
@@ -1,12 +1,12 @@
 App.launchScreens({
   // iOS
-  'iphone': 'resources/splash/splash-320x480.png',
-  'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-  'iphone5': 'resources/splash/splash-320x568@2x.png',
-  'ipad_portrait': 'resources/splash/splash-768x1024.png',
-  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-  'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+  // 'iphone': 'resources/splash/splash-320x480.png',
+  // 'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+  // 'iphone5': 'resources/splash/splash-320x568@2x.png',
+  // 'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  // 'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  // 'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  // 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
   'android_ldpi_portrait': 'public/image/closeup1mb.png',
