545f5c57d7b99e9f408319a533af29d3fa4b0a0d
eganpg <eganpg@gmail.com>
2015-01-17 19:41:16 -0800

    styling adjustments

---
+++ mobile-config.js
@@ -0,0 +1,20 @@
+App.launchScreens({
+  // iOS
+  'iphone': 'resources/splash/splash-320x480.png',
+  'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+  'iphone5': 'resources/splash/splash-320x568@2x.png',
+  'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+
+  // Android
+  'android_ldpi_portrait': 'public/image/closeup1mb.png',
+  'android_ldpi_landscape': 'public/image/closeup1mb.png',
+  'android_mdpi_portrait': 'public/image/closeup1mb.png',
+  'android_mdpi_landscape': 'public/image/closeup1mb.png',
+  'android_hdpi_portrait': 'public/image/closeup1mb.png',
+  'android_hdpi_landscape': 'public/image/closeup1mb.png',
+  'android_xhdpi_portrait': 'public/image/closeup1mb0.png',
+  'android_xhdpi_landscape': 'public/image/closeup1mb0.png'
+});
