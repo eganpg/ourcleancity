9399688181a3f9f367e79da48450e58fa83ae528
eganpg <eganpg@gmail.com>
2015-01-18 14:23:11 -0800

    add icon

--- mobile-config.js
+++ mobile-config.js
@@ -1,3 +1,16 @@
+App.icons({
+  // iOS
+  // 'iphone': 'resources/icons/icon-60x60.png',
+  // 'iphone_2x': 'resources/icons/icon-60x60@2x.png',
+  // 'ipad': 'resources/icons/icon-72x72.png',
+  // 'ipad_2x': 'resources/icons/icon-72x72@2x.png',
+
+  // Android
+  'android_ldpi': 'public/image/ourcleancity.png',
+  'android_mdpi': 'public/image/ourcleancity.png',
+  'android_hdpi': 'public/image/ourcleancity.png',
+  'android_xhdpi': 'public/image/ourcleancity.png'
+});
 App.launchScreens({
   // iOS
   // 'iphone': 'resources/splash/splash-320x480.png',
@@ -9,12 +22,12 @@ App.launchScreens({
   // 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
-  'android_ldpi_portrait': 'public/image/closeup1mb.png',
-  'android_ldpi_landscape': 'public/image/closeup1mb.png',
-  'android_mdpi_portrait': 'public/image/closeup1mb.png',
-  'android_mdpi_landscape': 'public/image/closeup1mb.png',
-  'android_hdpi_portrait': 'public/image/closeup1mb.png',
-  'android_hdpi_landscape': 'public/image/closeup1mb.png',
-  'android_xhdpi_portrait': 'public/image/closeup1mb0.png',
-  'android_xhdpi_landscape': 'public/image/closeup1mb0.png'
+  'android_ldpi_portrait': 'public/image/ourcleancity_load.png',
+  'android_ldpi_landscape': 'public/image/ourcleancity_load.png',
+  'android_mdpi_portrait': 'public/image/ourcleancity_load.png',
+  'android_mdpi_landscape': 'public/image/ourcleancity_load.png',
+  'android_hdpi_portrait': 'public/image/ourcleancity_load.png',
+  'android_hdpi_landscape': 'public/image/ourcleancity_load.png',
+  'android_xhdpi_portrait': 'public/image/ourcleancity_load.png',
+  'android_xhdpi_landscape': 'public/image/ourcleancity_load.png'
 });
