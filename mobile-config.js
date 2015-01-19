020b3c7051eb009be883b239b30cfe4f9c2dad56
eganpg <eganpg@gmail.com>
2015-01-18 16:02:09 -0800

    deployed to app store

--- mobile-config.js
+++ mobile-config.js
@@ -1,3 +1,8 @@
+App.info({
+  name: 'Our Clean City',
+  description: 'Public Works Made Easy',
+  version: '0.0.1'
+});
 App.icons({
   // iOS
   // 'iphone': 'resources/icons/icon-60x60.png',
@@ -31,3 +36,14 @@ App.launchScreens({
   'android_xhdpi_portrait': 'public/image/ourcleancity_load.png',
   'android_xhdpi_landscape': 'public/image/ourcleancity_load.png'
 });
+
+// meteor build ~/build-output-directory \ --server=ourcleancity.meteor.com
+// keytool -genkey -alias ourcleancity -keyalg RSA \-keysize 2048 -validity 10000
+// cd ~/build-output-directory/android/jarsigner -digestalg SHA1 unaligned.apk ourcleancity
+// ~/.meteor/android_bundle/android-sdk/build-tools/20.0.0/zipalign 4 \unaligned.apk production.apk
+
+// meteor build ~/build-output-directory \
+//     --server=ourcleancity.meteor.com
+
+// keytool -genkey -v -keystore my-release-key.keystore -alias ourcleancity -keyalg RSA -keysize 2048 -validity 10000
+// jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore unaligned.apk ourcleancity
